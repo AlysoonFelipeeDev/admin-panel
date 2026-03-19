7eb743ebf883508126271544a7f56e7102b1a785
jean <alysoonfelipe@hotmail.com>
2026-03-19 16:04:25 -0300

    feat: Cria schema de cadastro de usuario

--- src/schemas/authSchema.ts
+++ src/schemas/authSchema.ts
@@ -1,8 +1,14 @@
 import z from "zod";
 
 export const loginSchema= z.object({
-    email: z.string().email().min(1, "Email é obrigatório e/ou insira um email válido").trim(),
+    email: z.string().email("Coloque em formato de email ( email@emai.com)").min(1, "Email é obrigatório e/ou insira um email válido").trim(),
     password: z.string().min(6, "Senha é obrigatória").trim()
 })
 
+export const registerSchema= loginSchema.extend({
+    name: z.string().min(3, 'Nome tem que ter no minimo 3 caracteres'),
+    role: z.enum( ["admin", "member"])
+})
+
 export type AuthFormData = z.infer<typeof loginSchema>
+export type RegisterFormData = z.infer<typeof registerSchema>
