c1ab30278b899c5bd7d09c4d0223a022d147805c
jean <alysoonfelipe@hotmail.com>
2026-03-19 15:49:25 -0300

    feat: implementa mutação para criação de usuário no useUsers

--- src/hooks/useUsers.ts
+++ src/hooks/useUsers.ts
@@ -1,16 +1,29 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { usersService } from "../services/users";
+import type { User } from "../types/user";
+
 
 export function useUsers() {
+    const queryClient = useQueryClient()
 
     const query = useQuery({
         queryKey: ['users'],
         queryFn: () => usersService.getUsers()
     })
 
+    const createMutation = useMutation({
+        mutationFn: usersService.createUser,
+        onSuccess: () => {
+            queryClient.invalidateQueries({queryKey: ['users']})
+
+        }
+    })
+
     return {
         users: query.data ?? [],
         isLoading: query.isLoading,
-        isError: query.isError
+        isError: query.isError,
+        createUser: (user: Omit<User, 'id'>) => createMutation.mutate(user),
+        isCreating: createMutation.isPending
     }
 }
