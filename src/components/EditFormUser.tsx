7823147f1833600938b83fa37dac764ef16a85f9
jean <alysoonfelipe@hotmail.com>
2026-03-23 15:39:36 -0300

    feat: implementa formulario de edição do usuario logado

--- src/components/EditFormUser.tsx
+++ src/components/EditFormUser.tsx
@@ -1,24 +1,63 @@
 import styled from "styled-components"
 import { Button } from "./Ui/Button"
+import { useForm } from "react-hook-form"
+import { updateUserSchema, type EditProfileData } from "../schemas/authSchema"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useUsers } from "../hooks/useUsers"
+import { useUser } from "../contexts/AuthContext"
 
 export function EditFormUser(){
+    const { editUser } = useUsers()
+    const {user} = useUser()
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors }
+    } = useForm<EditProfileData>({
+        resolver: zodResolver(updateUserSchema)
+    })
+    
+    const onSubmit = (data: EditProfileData) => {
+        if(!user?.id) return
+        editUser({
+            id: user.id, 
+            user: {...user, ...data}
+        })
+    }
+
+    
     return (
         <Container>
-            <Form>
+            <Form onSubmit={handleSubmit(onSubmit)}>
                 <div>
                     <label htmlFor="name">Nome:</label>
-                    <input type="text" id="name"/>
+                    <input 
+                    type="text" 
+                    id="name"
+                    {...register('name')}
+                    />
+                    {errors.name && <span>{errors.name.message}</span>}
                 </div>
                 <div>
                     <label htmlFor="email"> Email:</label>
-                    <input type="email"  id="email" />
+                    <input 
+                    type="email" 
+                    id="email" 
+                    {...register('email')}
+                    />
+                    {errors.email && <span>{errors.email.message}</span>}
                 </div>
                 <div>
                     <label htmlFor="role">Função</label>
-                    <select id="role">
+                    <select 
+                    id="role"
+                    {...register('role')}
+                    >
                         <option value="admin">Administrador(a)</option>
                         <option value="memeber">Membro(a)</option>
                     </select>
+                    {errors.role && <span>{errors.role.message}</span>}
                 </div>
                 <Button type="submit">Atualizar</Button>
             </Form>
--- src/schemas/authSchema.ts
+++ src/schemas/authSchema.ts
@@ -10,5 +10,12 @@ export const registerSchema= loginSchema.extend({
     role: z.enum( ["admin", "member"])
 })
 
+export const updateUserSchema = z.object({
+    name: z.string().min(3, 'Nome tem que ter no minimo 3 caracteres'),
+    email: z.string().email("Coloque em formato de email ( email@emai.com)").min(1, "Email é obrigatório e/ou insira um email válido").trim(),
+    role: z.enum( ["admin", "member"])
+})
+
 export type AuthFormData = z.infer<typeof loginSchema>
 export type RegisterFormData = z.infer<typeof registerSchema>
+export type EditProfileData = z.infer<typeof updateUserSchema>
